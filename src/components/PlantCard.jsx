8da4daed995c8f4be0248345978b5aae7d383b95
Sanaya James <sanaya.j.james@gmail.com>
2025-12-04 23:44:21 -0700

    began to implement plant page display and search function

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function PlantCard() {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  //state will live here, app doesn't need to handle state based on the structure of the application
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  function fetchPlantList() {
+    fetch("http://localhost:6001/plants")
+      .then(r => {
+        if (!r.ok) {
+          throw new Error("Fetch request failed")
+        } else {
+          return r.json()
+        }
+      })
+      .then(data => {
+        console.log(data)
+        setPlants(data)
+      })
+      .catch(error => console.log("Fetch request failed", error))
+  }
+
+  useEffect(fetchPlantList, [])
+
+  const filteredPlants = plants.filter(plant =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
